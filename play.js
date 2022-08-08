b618ae25a75635b8be10dda7a1c42893cc1719db
Matthew Davis <matc.davis@gmail.com>
2022-08-08 18:09:56 +0000

    add ctrl c for exiting the game

--- play.js
+++ play.js
@@ -1,2 +1,24 @@
 const connect = require("./client");
 
+const handUserInput = (key) => {
+    if (key === '\u0003') {
+      console.log("you have exited the game")
+      process.exit();
+    }
+}
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+
+  stdin.on("data", handUserInput)
+
+  return stdin;
+};
+
+
+
+
+setupInput();
